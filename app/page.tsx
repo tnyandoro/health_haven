db9d7706cd35476ca214ac7abee64b025616b6f4
Tendai Nyandoro <tnyandoro@gmail.com>
2024-07-08 21:34:13 +0200

    Done with setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <div className="text-3xl underline">Welcome to the medical records app</div>
+    <div className="text-white text-3xl underline">Welcome to the medical records app</div>
   )
 }
