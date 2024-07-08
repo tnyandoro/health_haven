17cf10951534d695e945aed267b7465bfd183e59
Tendai Nyandoro <tnyandoro@gmail.com>
2024-07-08 22:54:44 +0200

    Add shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
-    <div className="text-white text-3xl underline">medical records app</div>
-  )
+    <>
+      <div className="text-white text-3xl underline">medical records app</div>
+      <Button className='text-white bg-blue-700'>Click me</Button>
+    </>
+  );
 }
