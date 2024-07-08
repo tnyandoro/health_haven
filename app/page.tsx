ba96b0eabf7170446fba80d2cee96c999f523ea7
Tendai Nyandoro <tnyandoro@gmail.com>
2024-07-08 23:05:16 +0200

    Add dark theme shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <div className="text-white text-3xl underline">medical records app</div>
-      <Button className='text-white bg-blue-700'>Click me</Button>
+      <Button >Click me</Button>
     </>
   );
 }
