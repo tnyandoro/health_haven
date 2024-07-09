cb477ed32846deb356585afedbc9b03f89eee40c
Tendai Nyandoro <tnyandoro@gmail.com>
2024-07-09 22:26:44 +0200

    Add destructure the control

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -5,16 +5,9 @@ import { useForm } from "react-hook-form"
 import { z } from "zod"
 
 import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+import { Form } from "@/components/ui/form"
+import CustomFormField from "../CustomFormField"
+
  
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -37,22 +30,13 @@ export const PatientForm = () => {
   }
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+        <section className="mb-12 space-y-4">
+          <h1 className="header">Hi there 🖐</h1>
+          <p className="text-dark-700"> Schedule your appointment</p>
+        </section>
+        <CustomFormField 
           control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
         />
         <Button type="submit">Submit</Button>
       </form>
