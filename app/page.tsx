d60577ef4254b557608e92623346c111e61f033e
Tendai Nyandoro <tnyandoro@gmail.com>
2024-07-09 14:15:36 +0200

    Add images for the home page

--- app/page.tsx
+++ app/page.tsx
@@ -26,6 +26,13 @@ export default function Home() {
           </div>
         </div>
       </section>
+      <Image 
+      src="/assets/images/onboarding-img.png"
+      height={1000}
+      width={1000}
+      alt="patient"
+      className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
