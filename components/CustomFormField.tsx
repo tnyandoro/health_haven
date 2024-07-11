343da6e251db0a0bf3fb75917417df7942a673fd
Tendai Nyandoro <tnyandoro@gmail.com>
2024-07-11 12:10:14 +0200

    Add props to get the input props

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -10,27 +10,41 @@ import {
     } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Control } from "react-hook-form"
+import { FormFieldType } from "./forms/PatientForm"
+import { Label } from "@radix-ui/react-label"
+import { ReactNode } from "react"
 
 interface CustomProps {
     control: Control<any>,
+    fieldType: FormFieldType,
+    name: string,
+    label?: string,
+    placeholder?: string,
+    iconSrc?: string,
+    iconAlt?: string,
+    disabled?: boolean,
+    placeHolder?: string,
+    dateFormat?: string,
+    showTimeSelect?: boolean,
+    children?: React.ReactNode,
+    renderSelecton?: (field: any) => React.ReactNode,
+
+
+
+
 }
 
-const CustomFormField = ({control}: CustomProps) => {
+const CustomFormField = ({ control, fieldType, name, label }: CustomProps) => {
 return (
     <FormField
         control={control}
-        name="username"
+        name={name}
         render={({ field }) => (
-        <FormItem>
-            <FormLabel>Username</FormLabel>
-            <FormControl>
-            <Input placeholder="shadcn" {...field} />
-            </FormControl>
-            <FormDescription>
-            This is your public display name.
-            </FormDescription>
-            <FormMessage />
-        </FormItem>
+            <FormItem className="flex-1">
+                {fieldType !== FormFieldType.CHECKBOX && label && (
+                    <FormLabel>{label}</FormLabel>
+                )}
+            </FormItem>
     )}
         />
     )
--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,14 +1,22 @@
 "use client"
- 
+
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
-
 import { Button } from "@/components/ui/button"
 import { Form } from "@/components/ui/form"
 import CustomFormField from "../CustomFormField"
 
- 
+export enum FormFieldType {
+  INPUT = 'input',
+  TEXTAREA = 'textarea',
+  PHONE_INPUT ='phoneInput',
+  CHECKBOX = 'checkbox',
+  DATE_PICKER = 'datePicker',
+  SELECT = 'select',
+  SKELETON = 'skeleton',
+}
+
 const formSchema = z.object({
   username: z.string().min(2, {
     message: "Username must be at least 2 characters.",
@@ -35,9 +43,9 @@ export const PatientForm = () => {
           <h1 className="header">Hi there 🖐</h1>
           <p className="text-dark-700"> Schedule your appointment</p>
         </section>
-        <CustomFormField 
-          control={form.control}
-        />
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control} name={"name"} label={"Full name"} placeholder={"John Doe"} iconSrc={"/assets/icons/user.svg"} iconAlt={"user"}        />
         <Button type="submit">Submit</Button>
       </form>
     </Form>
